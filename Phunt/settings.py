fc823c3c48ff47fc2937a81acd2ea44094af33bf
MdArifulislam21 <sabuj19977@gmail.com>
2021-07-06 16:59:16 +0600

    'chenged'

--- Phunt/settings.py
+++ Phunt/settings.py
@@ -136,6 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static'),
+
     os.path.join(BASE_DIR, 'Phunt/static/')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
